071555ca386db4a1d0cf468b851a76336c13153c
benpottier <benpottier@gmail.com>
2020-05-01 10:55:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='A cli for onetimesecret.com',
     author='Ben Pottier',
     author_email='benpottier@gmail.com',
+    python_requires='>3.6',
     packages=['ots'],
     install_requires=['fire'],
     scripts=['scripts/otscli.py']
