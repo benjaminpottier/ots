5fe3017b2a4ae4a4b7c6a7932ae40d08bfb88931
benpottier <benpottier@gmail.com>
2020-05-01 10:48:26 -0400

    Update otscli.py

--- scripts/otscli.py
+++ scripts/otscli.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 
+import os
+import sys
 import fire
 from ots import Ots
 
-user = ''
-api_key = ''
+credentials_file = '~/.ots'
+path = os.path.expanduser(credentials_file)
+
+if not os.path.exists(path):
+    print(f"Please create {path} with username:api_key.")
+    sys.exit(1)
+
+with open(path) as f:
+    user, api_key = f.read().rstrip().split(':')
 
 if __name__ == '__main__':
   fire.Fire(Ots(user, api_key))
+  
